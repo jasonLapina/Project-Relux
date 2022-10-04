ec52cdd27f0183424a162bb9030244290c94d701
jasonLapina <lapina.jason@gmail.com>
2022-10-04 16:58:03 +0800

    finish media queries

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -16,17 +16,21 @@ const FormOverlay = (props) => {
           <ion-icon onClick={props.onClose} name='close-outline' />
         </div>
         <h2 className={style.heading}>Start booking now</h2>
-        <h3>IMPORTANT PLEASE READ THESE TERMS BEFORE BOOKING</h3>
+        <h3 className={style.subheading}>
+          IMPORTANT PLEASE READ THESE TERMS BEFORE BOOKING
+        </h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
           repudiandae officia quod blanditiis? Rerum, deleniti. Aliquam soluta
-          pariatur suscipit consequuntur maiores quibusdam magnam. Odio nisi
-          excepturi aperiam, commodi eos minima! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Optio sunt facere quibusdam, repellat,
-          amet eveniet cupiditate eaque ad facilis, minus nostrum neque alias
-          aspe consectetur adipisicing elit. Optio sunt facere quibusdam,
-          repellat, amet eveniet cupiditate eaque ad facilis, minus nostrum
-          neque alias aspe
+          pariatur suscipit consequuntur agnam. Odio nisi excepturi aperiam,
+          commodi eos minima! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Optio sunt facere quibusdam, repellat, amet eveniet cupiditate
+          eaque ad facilis, minus nostrum neque alias aspe consectetur
+          adipisicing elit. Optio sunt facere quibusdam, repellat, amet eveniet
+          cupiditate eaque ad facilis, minus nostrum neque alias aspe. t facere
+          quibusdam, repellat, amet eveniet cupiditate eaque ad facilis, minus
+          lias aspe. t facere quibusdam, repellat, amet eveniet cupiditate eaque
+          ad facilis, minus nostrum neque alias aspe
         </p>
         <Button onClick={props.onClose}>Book now</Button>
       </div>
